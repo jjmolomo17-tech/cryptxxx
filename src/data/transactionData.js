4d8846781df5122e00fa9eee26e4a0c1f9cbba71
Joy Molomo <jjmolomo17@gmail.com>
2025-12-12 01:55:27 +0200

    added transactionData

---
+++ src/data/transactionData.js
@@ -0,0 +1,7 @@
+const transactions = [
+  { id: 1, date: "2025-12-01", type: "Buy", amount: "0.5 BTC", price: "$18,000" },
+  { id: 2, date: "2025-12-02", type: "Sell", amount: "0.2 BTC", price: "$19,000" },
+  { id: 3, date: "2025-12-03", type: "Buy", amount: "1 BTC", price: "$20,000" },
+];
+
+export default transactions;
