e362364d5d32d6a613752abd11e28ba4fce6268a
Joy Molomo <jjmolomo17@gmail.com>
2025-12-12 01:51:04 +0200

    changed main to src/main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // Entry point for React app
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "../App";
 import "./index.css"; // Tailwind styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
